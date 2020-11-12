d1886b09bd39b07a3047f0796e242081b5663604
jiahaiyang <Administrator@DESKTOP-Q729JTL>
2020-11-12 19:31:58 +0800

    参数修改

--- src/views/login/oidc/oidc-config.js
+++ src/views/login/oidc/oidc-config.js
@@ -1,10 +1,10 @@
 var config = {
-  authority: "http://192.168.0.101:8010",
+  authority: 'http://localhost:8010',
   client_id: 'js',
-  redirect_uri: "http://192.168.0.101:9527/#/signin-callback-oidc",
-  post_logout_redirect_uri: "http://192.168.0.101:9527",
+  redirect_uri: 'http://localhost:9527/#/signin-callback-oidc',
+  post_logout_redirect_uri: 'http://localhost:9527',
   response_type: 'code',
-  scope: "openid profile usercenterapi.read"
+  scope: 'openid profile usercenterapi.read'
 }
 
 export default config
